3d2f53ffcbb97beb03e71c6b54d8b2c51844d408
ravinder <ravinder50300@gmail.com>
2025-10-20 00:17:57 +0530

    add jenkinsfile

--- app/core/config.py
+++ app/core/config.py
@@ -1,31 +1,39 @@
-import os
-from typing import List
-from pydantic import ValidationError
+
+from typing import List, Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from functools import lru_cache
+
 
 
 class Settings(BaseSettings):
+    #CORE
     API_V1_STR: str = "/api/v1"
-    PROJECT_NAME: str = "SkillMatch"
+    PROJECT_NAME: str
     VERSION: str = "0.1.0"
     DEBUG: bool = True
+    ENVIRONMENT: str = "development"
+    # Network & CORS
     CORS_ORIGINS: List[str] = ["*"]
     ALLOWED_HOSTS: List[str] = ["*"]
-    ENVIRONMENT: str = "development"
-    ALGORITHM: str = "HS256"
+    #DATABASE
     DATABASE_URL: str
-    ACCESS_TOKEN_EXPIRY: int = 30        # minutes
-    REFRESH_TOKEN_EXPIRY: int = 1440     # 1 day
+    # Authentication & Security
+    ALGORITHM: str = "HS256"
     SECRET_KEY: str
-    OPENAI_API_KEY: str
-    REDIS_URL: str = "redis://localhost:6379/0"
+    ACCESS_TOKEN_EXPIRY: int = 30
+    REFRESH_TOKEN_EXPIRY: int = 1440
+
+    SECRET_KEY: str
+    REDIS_URL: str
     CACHE_EXPIRY: int = 300
-    AWS_S3_BUCKET: str | None = None
-    AWS_DEFAULT_REGION: str | None = None
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    # Optional AWS
+    AWS_S3_BUCKET: Optional[str] = None
+    AWS_DEFAULT_REGION: Optional[str] = None
 
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
 
 settings = Settings()
 
